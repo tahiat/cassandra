501fb4270b6bf916b465ab6cb4667421a8f79d64
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2022-11-25 18:13:13 +0100

    Moved static open methods for BigTableReader to the reader factory

--- test/unit/org/apache/cassandra/io/sstable/SSTableReaderTest.java
+++ test/unit/org/apache/cassandra/io/sstable/SSTableReaderTest.java
@@ -888,7 +888,7 @@ public class SSTableReaderTest
 
         expectedException.expect(CorruptSSTableException.class);
         expectedException.expectMessage("CompressionInfo.db");
-        SSTableReader.verifyCompressionInfoExistenceIfApplicable(desc, components);
+        desc.verifyCompressionInfoExistenceIfApplicable(components);
     }
 
     @Test
@@ -896,7 +896,7 @@ public class SSTableReaderTest
     {
         Descriptor desc = setUpForTestVerfiyCompressionInfoExistence();
         Set<Component> components = SSTable.discoverComponentsFor(desc);
-        SSTableReader.verifyCompressionInfoExistenceIfApplicable(desc, components);
+        desc.verifyCompressionInfoExistenceIfApplicable(components);
 
         // mark the toc file not readable in order to trigger the FSReadError
         File tocFile = new File(desc.filenameFor(Component.TOC));
@@ -904,7 +904,7 @@ public class SSTableReaderTest
 
         expectedException.expect(FSReadError.class);
         expectedException.expectMessage("TOC.txt");
-        SSTableReader.verifyCompressionInfoExistenceIfApplicable(desc, components);
+        desc.verifyCompressionInfoExistenceIfApplicable(components);
     }
 
     @Test
@@ -912,7 +912,7 @@ public class SSTableReaderTest
     {
         Descriptor desc = setUpForTestVerfiyCompressionInfoExistence();
         Set<Component> components = SSTable.discoverComponentsFor(desc);
-        SSTableReader.verifyCompressionInfoExistenceIfApplicable(desc, components);
+        desc.verifyCompressionInfoExistenceIfApplicable(components);
     }
 
     private Descriptor setUpForTestVerfiyCompressionInfoExistence()
