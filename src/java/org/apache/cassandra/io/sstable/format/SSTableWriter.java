a32dddfc81b547bbdd8d2b3214ec20b20b01afda
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2023-01-03 09:47:41 +0100

    Extracted non-big-table-specific functionality from BigTableWriter to SortedTableWriter

--- src/java/org/apache/cassandra/io/sstable/format/SSTableWriter.java
+++ src/java/org/apache/cassandra/io/sstable/format/SSTableWriter.java
@@ -44,7 +44,6 @@ import org.apache.cassandra.io.sstable.Component;
 import org.apache.cassandra.io.sstable.Descriptor;
 import org.apache.cassandra.io.sstable.SSTable;
 import org.apache.cassandra.io.sstable.SSTableBuilder;
-import org.apache.cassandra.io.sstable.format.big.RowIndexEntry;
 import org.apache.cassandra.io.sstable.metadata.MetadataCollector;
 import org.apache.cassandra.io.sstable.metadata.MetadataComponent;
 import org.apache.cassandra.io.sstable.metadata.MetadataType;
@@ -207,7 +206,7 @@ public abstract class SSTableWriter extends SSTable implements Transactional
      *
      * @throws FSWriteError if a write to the dataFile fails
      */
-    public abstract RowIndexEntry append(UnfilteredRowIterator iterator);
+    public abstract AbstractRowIndexEntry append(UnfilteredRowIterator iterator);
 
     public abstract long getFilePointer();
 
